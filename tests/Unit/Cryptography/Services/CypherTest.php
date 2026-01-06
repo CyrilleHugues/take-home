36ff742b3581c4215adac97350fdba68e2fc4266
Cyrille Hugues <cyrille.hugues@gmail.com>
2026-01-06 18:55:59 +0100

    refactor(tests): use provider for maintanability

--- tests/Unit/Cryptography/Services/CypherTest.php
+++ tests/Unit/Cryptography/Services/CypherTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Unit\Cryptography\Services;
 
 use App\Cryptography\Services\Base64Strategy;
 use App\Cryptography\Services\Cypher;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 
@@ -17,58 +18,94 @@ class CypherTest extends TestCase
         $this->cypher = new Cypher(new Base64Strategy());
     }
 
-    public function testItWillReturnEncryptedSimpleData(): void
+    public static function encryptionProvider(): array
     {
-        $data = ['test' => 'thing to encrypt'];
-
-        $this->assertEquals(['test' => 'dGhpbmcgdG8gZW5jcnlwdA=='], $this->cypher->encrypt($data));
-    }
-
-    public function testItWillReturnEncryptedMixedData(): void
-    {
-        $data = ['first' => 'thing to encrypt', 'second' => 'another thing', 'third' => ['sub' => 'array']];
-
-        $this->assertEquals(
-            [
-                'first' => 'dGhpbmcgdG8gZW5jcnlwdA==',
-                'second' => 'YW5vdGhlciB0aGluZw==',
-                'third' => 'eyJzdWIiOiJhcnJheSJ9',
+        return [
+            'simple case' => [
+                ['test' => 'thing to encrypt /'],
+                ['test' => 'dGhpbmcgdG8gZW5jcnlwdCAv'],
             ],
-            $this->cypher->encrypt($data)
-        );
-    }
-
-    public function testItWillDecryptAFullyEncryptedPayload(): void
-    {
-        $data = ['test' => 'dGhpbmcgdG8gZW5jcnlwdA=='];
-
-        $this->assertEquals(
-            ['test' => 'thing to encrypt'],
-            $this->cypher->decrypt($data),
-        );
-    }
-
-    public function testItWillDecryptAPartiallyEncryptedPayload(): void
-    {
-        $data = ['first' => 'dGhpbmcgdG8gZW5jcnlwdA==', 'second' => "john@example.com"];
-
-        $this->assertEquals(
-            ['first' => 'thing to encrypt', 'second' => "john@example.com"],
-            $this->cypher->decrypt($data),
-        );
-    }
-
-    public function testItWillDecryptObjectsToo(): void
-    {
-        $data = [
-            'first' => 'dGhpbmcgdG8gZW5jcnlwdA==',
-            'second' => 'YW5vdGhlciB0aGluZw==',
-            'third' => 'eyJzdWIiOiJhcnJheSJ9',
+            'handling sub objects' => [
+                ['first' => 'thing to encrypt', 'second' => 'another thing', 'third' => ['sub' => 'array']],
+                ['first' => 'dGhpbmcgdG8gZW5jcnlwdA==', 'second' => 'YW5vdGhlciB0aGluZw==', 'third' => 'eyJzdWIiOiJhcnJheSJ9']
+            ],
+            'example' => [
+                [
+                    'name' => 'John Doe',
+                    'age' => 30,
+                    'contact' => [
+                        "email" => "john@example.com",
+                        "phone" => "123-456-7890"
+                    ],
+                ],
+                [
+                    'name' => 'Sm9obiBEb2U=',
+                    'age' => 'MzA=',
+                    'contact' => 'eyJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20iLCJwaG9uZSI6IjEyMy00NTYtNzg5MCJ9',
+                ],
+            ]
         ];
+    }
 
-        $this->assertEquals(
-            ['first' => 'thing to encrypt', 'second' => 'another thing', 'third' => ['sub' => 'array']],
-            $this->cypher->decrypt($data)
-        );
+    #[DataProvider('encryptionProvider')]
+    public function testEncryption(array $input, array $expected): void
+    {
+        $this->assertEquals($expected, $this->cypher->encrypt($input));
+    }
+
+    public static function decryptionProvider(): array
+    {
+        return [
+            'simple case' => [
+                ['test' => 'dGhpbmcgdG8gZW5jcnlwdCAv'],
+                ['test' => 'thing to encrypt /'],
+            ],
+            'handling sub objects' => [
+                ['first' => 'dGhpbmcgdG8gZW5jcnlwdA==', 'second' => 'YW5vdGhlciB0aGluZw==', 'third' => 'eyJzdWIiOiJhcnJheSJ9'],
+                ['first' => 'thing to encrypt', 'second' => 'another thing', 'third' => ['sub' => 'array']],
+            ],
+            'partially encrypted' => [
+                ['first' => 'dGhpbmcgdG8gZW5jcnlwdA==', 'second' => "john@example.com"],
+                ['first' => 'thing to encrypt', 'second' => "john@example.com"],
+            ],
+            'example1' => [
+                [
+                    'name' => 'Sm9obiBEb2U=',
+                    'age' => 'MzA=',
+                    'contact' => 'eyJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20iLCJwaG9uZSI6IjEyMy00NTYtNzg5MCJ9',
+                ],
+                [
+                    'name' => 'John Doe',
+                    'age' => 30,
+                    'contact' => [
+                        "email" => "john@example.com",
+                        "phone" => "123-456-7890"
+                    ],
+                ],
+            ],
+            'example2' => [
+                [
+                    'name' => 'Sm9obiBEb2U=',
+                    'age' => 'MzA=',
+                    'contact' => 'eyJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20iLCJwaG9uZSI6IjEyMy00NTYtNzg5MCJ9',
+                    "birth_date" => "1998-11-19",
+                ],
+                [
+                    'name' => 'John Doe',
+                    'age' => 30,
+                    'contact' => [
+                        "email" => "john@example.com",
+                        "phone" => "123-456-7890"
+                    ],
+                    "birth_date" => "1998-11-19",
+                ],
+            ],
+        ];
+    }
+
+    #[DataProvider('decryptionProvider')]
+    public function testDecryption(array $input, array $expected): void
+    {
+        $this->assertEquals($expected, $this->cypher->decrypt($input));
     }
 }
